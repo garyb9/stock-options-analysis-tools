6d69eb8b65de4cedfc811bd596c748800cc98b37
Gary Berkinfand <gberkinfand@gmail.com>
2020-12-09 00:25:54 +0200

    add pltr
    Signed-off-by: Gary Berkinfand <gberkinfand@gmail.com>

---
+++ OptionsAnalysis/src/PLTR.py
@@ -0,0 +1,21 @@
+from OptsAnalysis import OptsAnalysis
+
+# SPY (S&P 500 ETF) example
+filePath = r'../data/pltr.xls'
+Opts = OptsAnalysis()
+# Opts.BuildFromTS(filePath)
+Opts.BuildFromWeb('PLTR')
+
+for date in Opts.GetExpirationDates():
+    Opts.PlotHistByDate(date, 'Both')
+
+"""
+Opts.PrintExpirationDates()
+expDates = Opts.GetExpirationDates()
+Opts.PlotHistByDate(expDates[0], 'Volume')
+Opts.PlotHistByDate(expDates[0], 'OpenInt')
+Opts.PlotHistByDate(expDates[0], 'Both')
+Opts.PlotHist('Volume')
+Opts.PlotHist('OpenInt')
+Opts.PlotHist('Both')
+"""
