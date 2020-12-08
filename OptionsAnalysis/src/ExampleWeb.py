ac630602611693bfbb6fddb9bc48809df527db87
Gary Berkinfand <gberkinfand@gmail.com>
2020-12-09 00:00:45 +0200

    web fix
    Signed-off-by: Gary Berkinfand <gberkinfand@gmail.com>

--- OptionsAnalysis/src/ExampleWeb.py
+++ OptionsAnalysis/src/ExampleWeb.py
@@ -5,3 +5,8 @@ from OptsAnalysis import OptsAnalysis
 Opts = OptsAnalysis()
 Opts.BuildFromWeb("PLTR")
 Opts.PrintExpirationDates()
+
+expDates = Opts.GetExpirationDates()
+Opts.PlotHistByDate(expDates[0], 'Volume')
+Opts.PlotHistByDate(expDates[0], 'OpenInt')
+Opts.PlotHistByDate(expDates[0], 'Both')
